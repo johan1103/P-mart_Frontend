0faa0b00dc0dac0e87c5fa1bb14eb883337b4f71
johan1103 <71641610+johan1103@users.noreply.github.com>
2020-10-06 05:35:54 +0900

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Home">
+    home page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
---
+++ src/views/Shop.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        안녕친구들
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
