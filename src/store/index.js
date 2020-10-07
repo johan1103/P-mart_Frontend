4f5d14187caa7f0a6560683ff160e6ed598459b1
KTH <johan1103@naver.com>
2020-10-08 01:50:27 +0900

    vuelecture6ver

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import banner from './banner/index';
+import product from './product/index';
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules:{
-    banner
+    banner,
+    product
   },
   state: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="Home">
     <SlideBanner />
+    <BestProductList />
   </div>
 </template>
 
 <script>
 import SlideBanner from '@/components/home/SlideBanner.vue';
+import BestProductList from '@/components/home/BestProductList.vue';
 
 export default {
   name: 'home',
   components: {
-    SlideBanner
+    SlideBanner,
+    BestProductList
   }
 }
 </script>
