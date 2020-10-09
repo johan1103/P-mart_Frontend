45afc950caf404dd2620e912e9684bdd27deb433
KTH <johan1103@naver.com>
2020-10-10 05:01:41 +0900

    vueLecture8th-1

---
+++ src/components/shop/CategoryFilter.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div>
+
+   <ul class="p-b-54">
+        <li class="p-t-4">
+                <a href="#" class="s-text13 active1">
+                    All
+                </a>
+        </li>
+
+        <li class="p-t-4">
+            <a href="#" class="s-text13">
+                Women
+            </a>
+        </li>
+
+        <li class="p-t-4">
+            <a href="#" class="s-text13">
+                Men
+            </a>
+        </li>
+
+        <li class="p-t-4">
+            <a href="#" class="s-text13">
+                Kids
+            </a>
+        </li>
+
+        <li class="p-t-4">
+            <a href="#" class="s-text13">
+                Accesories
+            </a>
+        </li>
+    </ul>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
