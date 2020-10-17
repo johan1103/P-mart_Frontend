3aa6243fa352bbd16637a56dcfaf2a086ab2c8e0
KTH <johan1103@naver.com>
2020-10-17 11:11:06 +0900

    add checkboxAnimation-2

--- src/components/shop/ProductList.vue
+++ src/components/shop/ProductList.vue
@@ -30,7 +30,22 @@ export default {
     },
 
     created(){
-        this.$store.dispatch('product/setProducts');
+        this.$store.dispatch('product/setProducts').then(()=>{
+// add animation checkicon
+			$('.block2-btn-addcart').each(function(){
+				var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+				$(this).on('click', function(){
+				swal(nameProduct, "is added to cart !", "success");
+				});
+			});
+		
+			$('.block2-btn-addwishlist').each(function(){
+				var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+				$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+				});
+			});
+        })
     },
     components: {
         Product
