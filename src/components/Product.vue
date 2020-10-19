9acbf81894274c430b249c6bf933dc5d6a944f7e
KTH <johan1103@naver.com>
2020-10-19 15:57:56 +0900

    reapir checkboxAnimation

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -41,7 +41,21 @@ export default {
     props: ['product'],
 	methods: {
 		addToCart(product){
-			this.$store.dispatch('cart/addItem',product);
+			this.$store.dispatch('cart/addItem',product).then(()=>{
+                			$('.block2-btn-addcart').each(function(){
+				var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+				$(this).on('click', function(){
+				swal(nameProduct, "is added to cart !", "success");
+				});
+			});
+		
+			$('.block2-btn-addwishlist').each(function(){
+				var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+				$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+				});
+			});
+            })
 		}
     }
 }
--- src/components/shop/CategoryFilter.vue
+++ src/components/shop/CategoryFilter.vue
@@ -4,31 +4,31 @@
 
    <ul class="p-b-54">
         <li class="p-t-4">
-                <a href="#" class="s-text13 active1" @click="setCategory(0)">
+                <a href="#" class="s-text13 active1" @click="setCategory(0)" active-class="sale-noti" exact>
                     All
                 </a>
         </li>
 
         <li class="p-t-4">
-            <a href="#" class="s-text13" @click="setCategory(1)">
+            <a href="#" class="s-text13" @click="setCategory(1)" active-class="sale-noti" exact>
                 Women
             </a>
         </li>
 
         <li class="p-t-4">
-            <a href="#" class="s-text13" @click="setCategory(2)">
+            <a href="#" class="s-text13" @click="setCategory(2)" active-class="sale-noti" exact>
                 Men
             </a>
         </li>
 
         <li class="p-t-4">
-            <a href="#" class="s-text13" @click="setCategory(3)">
+            <a href="#" class="s-text13" @click="setCategory(3)" active-class="sale-noti" exact>
                 Kids
             </a>
         </li>
 
         <li class="p-t-4">
-            <a href="#" class="s-text13" @click="setCategory(4)">
+            <a href="#" class="s-text13" @click="setCategory(4)" active-class="sale-noti" exact>
                 Accesories
             </a>
         </li>
